d7952007f777d6194f491967d847642841e4178e
Jimi <jimi.vainikainen@gmail.com>
2026-02-28 22:15:40 +0200

    added more login tests and removed other tests

--- tests/helpers/cookies.ts
+++ tests/helpers/cookies.ts
@@ -1,4 +1,4 @@
-import { Page } from '@playwright/test';
+import type { Page } from '@playwright/test';
 
 export async function dismissCookies(page: Page): Promise<void> {
     const modal = page.locator('[data-testid="cookie-consent-modal"]');
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,6 +3,14 @@ import { dismissCookies } from './helpers/cookies.ts';
 import { log } from 'console';
 
 const BASE = 'https://www.vr.fi/en';
+function generateRandomEmail()
+{
+    const randomName_1 = Math.random().toString(36).substring(2,10);
+    const randomName_2 = Math.random().toString(36).substring(2,10);
+    const email = `${randomName_1}.${randomName_2}@example.com`;
+    return email;
+}
+
 test.describe('Login', () => {
     test.beforeEach(async ({ page }) => {
         await page.goto(BASE);
@@ -50,14 +58,12 @@ test.describe('Login', () => {
             'Log in - VR'
         );
 
-        await page.getByRole('textbox', { name: 'Email' }).fill('test.test@gmail.com');
-        await expect(page.getByRole('textbox', { name: 'Email' })).toHaveValue('test.test@gmail.com');
+        const email = generateRandomEmail();
+        await page.locator('#email').fill(email);
+        await expect(page.locator('#email')).toHaveValue(email);
 
         // test show/hide pass is visible when clicked and the visual is changed
-        await page.getByRole('textbox', { name: 'Password' }).fill('password');
-        await expect(page.getByRole('button', { name: 'Show password' })).toBeVisible();
-        await page.getByRole('button', { name: 'Show password' }).click();
-        await expect(page.getByRole('button', { name: 'Hide password' })).toBeVisible();
+        await page.locator('#password').fill('password');
         
         await page.getByRole('button', { name: 'Log in', exact: true }).click();
 
@@ -74,5 +80,27 @@ test.describe('Login', () => {
         await expect(modalLabel).toBeHidden();
         await expect(modalDescription).toBeHidden();
         await expect(modalButton).toBeHidden();
+    });
+
+    test('check that password text functions correctly', async ({ page }) => {
+        await page.getByTestId('login-link').click();
+        await expect(page).toHaveTitle(
+            'Log in - VR'
+        );
+        const email = generateRandomEmail();    
+        const emailInput = page.locator('#email');
+        await emailInput.fill(email);
+        await expect(emailInput).toHaveValue(email);
+
+        // test show/hide pass is visible when clicked and the visual is changed
+        const passwordInput = page.locator('#password')
+        await passwordInput.fill('password');
+        await expect(passwordInput).toHaveValue('password');
+
+        await expect(page.getByRole('button', { name: 'Show password' })).toBeVisible();
+        await expect(passwordInput).toHaveAttribute('type','password');
+        await page.getByRole('button', { name: 'Show password' }).click();
+        await expect(passwordInput).toHaveAttribute('type','text');
+        await expect(page.getByRole('button', { name: 'Hide password' })).toBeVisible();
     });
 });
